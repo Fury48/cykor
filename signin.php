e68ffe35d1e7b0557e69eea00af634d922aef6ac
Fury48 <gamingfury48@gmail.com>
2025-05-16 01:45:22 +0900

    가입 기능 구현(중복방지 및 페이지 이동)

---
+++ signin.php
@@ -0,0 +1,28 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'cykor';
+
+$dbconn = new mysqli($host,$username,$password,$database);
+if(!$dbconn) die("DB연결 실패");
+
+//id,pw 입력 받고 변수에 저장
+$sid = $_POST['sid'];
+$spw = $_POST['spw'];
+
+//id 중복 체크
+$jungbok = "SELECT * FROM userinfo WHERE id = '$sid'";
+$result = $dbconn->query($jungbok);
+
+if($result->num_rows>0) {
+    echo '<script>alert("이미 사용중인 ID입니다.");window.location.href="signin.html";</script>';
+}
+else {
+    $signin = "INSERT INTO userinfo (id,pw) VALUES ('$sid','$spw')";
+    if ($dbconn->query($signin) == TRUE) {
+        echo '<script>alert("가입이 완료되었습니다.");window.location.href="login.html";</script>';
+    }
+}
+$dbconn->close();
+?>
