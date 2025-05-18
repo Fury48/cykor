ecbd1f7a52d7372c2ba6631a9f80ddc83b157dc2
Fury48 <gamingfury48@gmail.com>
2025-05-18 16:43:23 +0900

    게시글 출력 구현현

--- board.php
+++ board.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'cykor';
+
+$dbconn = new mysqli($host,$username,$password,$database);
+if(!$dbconn) die("DB연결 실패");
+//게시글 가져오기
+$list = "SELECT unid, title, context, writer FROM board";
+$result = $dbconn->query($list);
+
+$unid = $_GET['unid'];
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,3 +27,14 @@
         <a href="signin.html">sign in</a>
         <a href="logout.php">log out</a>
     </div>
+    <div>
+        <?php 
+        $page = "SELECT * FROM board WHERE unid = $unid";
+        $result = $dbconn->query($page);
+        $row = $result->fetch_assoc();
+        echo "<h4>".$row['title']."</h4>";
+        echo "<p>".$row['context']."</p>";
+        ?>
+    </div>
+</body>
+</html>
